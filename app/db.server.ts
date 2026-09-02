226a52b9fa1e3736cde05cf00d27a04bd19fa46a
agijaipur <androgamesinfotech@gmail.com>
2026-09-02 20:13:42 +0530

    Support MONGODB_URI env var automatically in db.server.ts

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,5 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 
+// Support both MONGODB_URI and DATABASE_URL
+if (!process.env.DATABASE_URL && process.env.MONGODB_URI) {
+  process.env.DATABASE_URL = process.env.MONGODB_URI;
+}
+
 declare global {
   // eslint-disable-next-line no-var
   var prismaGlobal: PrismaClient;
